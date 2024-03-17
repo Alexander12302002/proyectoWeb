51c5db1d458dc41bad7e1c9b4705d8b25d34f238
Aki_1130 <106930173+Alexander12302002@users.noreply.github.com>
2024-03-16 19:50:28 -0500

    feat: :construction: mobile adaptability in progress

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
-const menu = document.querySelector("#menu")
-const abrir = document.querySelector("#abrir")
-const cerrar = document.querySelector("#cerrar")
+const menu = document.querySelector("#menu");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const opcionesMenu = document.querySelectorAll(".menu-items a");
 
 abrir.addEventListener("click", () => {
-    menu.classList.add("visible")
-})
+    menu.classList.add("visible");
+});
 
 cerrar.addEventListener("click", () => {
-    menu.classList.remove("visible")
-})
+    menu.classList.remove("visible");
+});
+
+opcionesMenu.forEach(opcion => {
+    opcion.addEventListener("click", () => {
+        menu.classList.remove("visible"); 
+    });
+});
